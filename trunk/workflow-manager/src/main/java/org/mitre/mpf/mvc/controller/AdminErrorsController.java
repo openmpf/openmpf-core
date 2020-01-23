5eb5a4f26559ad9c79049b5022d29c16bf31e73c
Brian Rosenberg <brosenberg@mitre.org>
2020-01-23 16:33:23 -0500

    Test removing website profile

--- trunk/workflow-manager/src/main/java/org/mitre/mpf/mvc/controller/AdminErrorsController.java
+++ trunk/workflow-manager/src/main/java/org/mitre/mpf/mvc/controller/AdminErrorsController.java
@@ -26,22 +26,20 @@
 
 package org.mitre.mpf.mvc.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @Scope("request")
-@Profile("website")
-public class AdminErrorsController 
-{	
+public class AdminErrorsController
+{
 	private static final Logger log = LoggerFactory.getLogger(AdminErrorsController.class);
 
 	public static final String DEFAULT_ERROR_VIEW = "error";
